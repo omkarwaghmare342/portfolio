9b8bb3c5933e004e64187921f119e91c7f5fef2a
Onkar Waghmare <onkar@astrophelaerospace.in>
2025-06-22 19:14:37 +0530

    Initial commit

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -30,7 +30,7 @@ const Navbar = () => {
           <NavLink href='#education'>Education</NavLink>
         </NavItems>
         <ButtonContainer>
-          <GitHubButton href={Bio.github} target="_blank">Github Profile</GitHubButton>
+          <GitHubButton href={Bio.linkedin} target="_blank">LinkedIn Profile</GitHubButton>
         </ButtonContainer>
         {
           isOpen &&
@@ -50,7 +50,7 @@ const Navbar = () => {
             <MobileLink href='#education' onClick={() => {
               setIsOpen(!isOpen)
             }}>Education</MobileLink>
-            <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.github} target="_blank">Github Profile</GitHubButton>
+            <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.linkedin} target="_blank">LinkedIn Profile</GitHubButton>
           </MobileMenu>
         }
       </NavbarContainer>
--- src/data/constants.js
+++ src/data/constants.js
@@ -6,7 +6,7 @@ export const Bio = {
     "To Build a long-term career as Electronics Embedded Engineer with opportunities for career growth.",
   github: "https://github.com/omkarwaghmare342",
   resume:
-    "https://drive.google.com/file/d/1pCdLjdfcV-lfXNsT3lnyg2xNwLqtSDu8/view?usp=sharing",
+ "https://drive.google.com/file/d/1erQ20FgDdl_G0_AuBJtJXY8Wmcvz5j5H/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/omkar-waghmare-696a64231/",
   email: "omkarwaghmare843@gmail.com",
 };
@@ -15,8 +15,9 @@ export const skills = [
   {
     title: "Embedded Software",
     skills: [
-      { name: "KeiluVision ", image: "https://embeddedtools.store/wp-content/uploads/2024/02/Prod-uVision600x600.png" },
+      { name: "KeiluVision ", image: "https://i.postimg.cc/V6KRJ90p/images-1.jpg" },
       { name: "Embedded C,C++", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/926px-C_Programming_Language.svg.png" },
+      {name: "STM32 Cube IDE", image: "https://i.postimg.cc/nhmHF09R/image-272-3x.png"},
       { name: "PCB Designing", image: "https://raw.githubusercontent.com/github/explore/7af95003139e68a3a54e382bb4f23a72836ef348/topics/altium-designer/altium-designer.png" },
       { name: "Arduino IDE", image: "https://e7.pngegg.com/pngimages/676/338/png-clipart-arduino-computer-software-library-electronics-computer-electronics-baby-thumbnail.png" },
       { name: "Platform IO", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/PlatformIO_logo.svg/768px-PlatformIO_logo.svg.png" },
@@ -50,7 +51,7 @@ export const experiences = [
     id: 0,
     role: "Embedded-IIOT engineer",
     company: "ICon Labs(Autovue Electronics)",
-    date: "June 2024 to June 2023",
+    date: "June 2024 to June 2024",
     desc: `• I completed a 1-month internship at Icon Labs in which I worked on embedded systems, IoT,
     Python and HTML for dashboard design, etc. I worked with MQTT local servers to transmit
     data from PLC controllers using RS485 (Modbus) to dashboards via ESP32. Additionally, I
@@ -59,13 +60,26 @@ export const experiences = [
     skills: ["PCB design", "MQTT(Mosquitto Broker)", "ModBus(RS485)", "PythonFlask", "canvas.js",],
     doc: "",
   },
+  {
+    id: 1,
+    role: "Associate Avionics Intern",
+    company: "Astrophel Aerospace Pvt.Ltd",
+    date: "Feb 25 to Present",
+    desc: `• I am currently working at Astrophel Aerospace Pvt. Ltd. 
+    as an Associate Avionics Intern, 
+    where I am involved in the development and testing of avionics systems. 
+    My work includes hands-on experience with embedded systems 
+    and electronics used in aerospace applications.`,
+    skills: ["STM32 Cube IDE", "Free RTOS", "Interrupt HAndling", "Xilinx Zynq",],
+    doc: "",
+  },
 ];
 
 export const education = [
   {
     id: 0,
-    img: "https://prolearnindia.com/wp-content/uploads/2023/03/3-4.jpg",
-    school: "Savitribai Phule Pune University",
+    img: "https://i.postimg.cc/wTr7NY1F/images.jpg",
+    school: "Pune Institute Of Computer Technology",
     date: "Sept 2023 - Ongoing",
     grade: "First Class",
     desc: "I am pursuing a Bachelor of Engineering from Pune University with a specialization in Electronics and Telecommunication.",
@@ -91,7 +105,7 @@ export const projects = [
     image: "https://i.postimg.cc/8CgwsfkG/bot.jpg",
     tags: ["ESP32", "AI", "Voice Processing", "Embedded Systems"],
     category: "IoT & AI",
-    github: "https://github.com/omkarwaghmare342/AI-Bot",
+    github: "https://github.com/omkarwaghmare342/Otto-AI-BOT",
   },
   {
     id: 5,
@@ -101,7 +115,7 @@ export const projects = [
     image: "https://i.postimg.cc/cJ9QNvz0/image.png",
     tags: ["ESP8266", "IoT", "Firebase", "AI","Sensors","pH sensor","Agreeculture"],
     category: "IoT & Environmental Monitoring",
-    github: "https://github.com/omkarwaghmare342/AI-Bot",
+    github: "https://github.com/omkarwaghmare342/Aquaculture_monitoring",
   },
   {
     id: 7,
@@ -112,5 +126,15 @@ export const projects = [
     tags: ["ESP32", "MQTT", "Grafana", "Embedded Systems"],
     category: "IOT & MQTT",
     github: "https://github.com/omkarwaghmare342/AI-Bot",
+  },
+  {
+    id: 8,
+    title: "Optical Distance Measurement System",
+    date: "January 2025 to January 2025",
+    description: "Developed a battery-operated distance measurement system using the GY-30 light sensor and an OLED display. The device calculates distances based on light intensity variations and displays results in real time, making it suitable for portable applications.",
+    image: "https://i.postimg.cc/q7p55nvk/Optical-Distance-Measurement-System.jpg",
+    tags: ["GY-30", "OLED", "Battery Operated", "Embedded Systems"],
+    category: "Embedded Hardware",
+    github: "https://github.com/omkarwaghmare342/Optical_Distance_Measurement_System",
   }
 ];
